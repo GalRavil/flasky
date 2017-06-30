4ed085f6a2e667c6bdd48901d112c16328681f45
GalRavil <gal.ravil93@gmail.com>
2017-06-30 09:37:45 +0300

    let's continue

---
+++ app/models.py
@@ -0,0 +1,192 @@
+fom datetime import datetime
+
+from flask import current_app
+from flask_login import UserMixin, AnonymousUserMixin
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db, login_manager
+
+
+# application permissions:
+#     0b00000001 (0x01)   follow users
+#     0b00000010 (0x02)   comment on articles written by others
+#     0b00000100 (0x04)   write articles
+#     0b00001000 (0x08    moderate comments made by others
+#     0b10000000 (0x80)   administration access
+
+# user roles:
+#     0b00000000 (0x00)   anonymous - user who is not loggen in. read-only access 
+#                                     to the app
+#     0b00000111 (0x07)   user - basic permissions to write articles and comments
+#                                 and to follow other users. this is default for new users
+#     0b00001111 (0x0f)   moderator - adds permission to suppress comments deemed 
+#                                 offensive or inappropriate
+#     0b11111111 (0xff)   amninistrator - full access, which includes permission 
+#                                 to change the roles of other users 
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRTITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.String(64), default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User':         (Permission.FOLLOW | 
+                            Permission.COMMENT |
+                            Permission.WRTITE_ARTICLES,
+                            True),
+
+            'Moderator':    (Permission.FOLLOW | 
+                            Permission.COMMENT |
+                            Permission.WRTITE_ARTICLES,
+                            True),
+
+            'Administrator':(0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return '<Role {!r}>'.format(self.name)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.String(64), primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.Datetime(), default=datetime.utcnow)
+    last_seen = db.Column(db.Datetime(), default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASK_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = db.session.add(self)
+        return True
+
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id})
+
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
+
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dump({'change_email': self.id, 'new_email': new_email})
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        
+        if new_email is None:
+            return False
+
+        # if email is already in use
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+
+    def can(self, permissions):
+        return role is not None and \
+            (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return slef.can(Permission.ADMINISTER)
+
+    # must be called each time a requets from user is received
+    # in @auth.before_app_request
+    def ping(self):
+        self.last_seen = dateitme.utcnow()
+        db.session.add(self)
+
+    def __repr__(self):
+    	return '<User {!r}>'.format(self.username)
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
