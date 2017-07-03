5e13d5bb371a6ba1f4f2065cd260db372f02defd
GalRavil <gal.ravil93@gmail.com>
2017-07-03 16:55:56 +0300

    + method to handle rich text on server side

--- app/models.py
+++ app/models.py
@@ -4,7 +4,9 @@ import hashlib
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 
+import bleach
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from markdown import markdown
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db, login_manager
@@ -240,6 +242,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -259,3 +262,20 @@ class Post(db.Model):
                 )
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = [
+            'a', 'abbr', 'acronym', 'b', 'blockquote',
+            'code', 'em', 'i', 'li', 'ol', 'pre', 'strong',
+            'ul', 'h1', 'h2', 'h3', 'h4', 'h5', 'p'
+        ]
+        target.body_html = bleach.linkify(bleach.clean(
+                markdown(value, output_format='html'),
+                tags=allowed_tags, strip=True
+            ))
+
+# Function on_change_body is registered as a listener of sqlalchemy's 'set'
+# event. It will be automatically invoken whenever the body field on any 
+# instance of the class is set to a new value
+db.event.listen(Post.body, 'set', Post.on_changed_body)
